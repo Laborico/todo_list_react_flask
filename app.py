a43fca614d36fdc5798c912fa8bf5d9f92fd67c3
David Laborico <david.laborico@michelada.io>
2022-03-18 22:34:45 -0500

    Added heroku config

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import (Flask, render_template)
 
-app = Flask("__main__")
+app = Flask(__name__)
 
 @app.route("/")
 def my_index():
     return render_template("index.html", flask_token="Hello   world")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
